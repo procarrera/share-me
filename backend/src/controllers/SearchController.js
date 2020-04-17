76499e6fe12dc55ce79a0e6679378c8ec30b8d23
Pedro Carrera <procarrera@gmail.com>
2020-04-17 01:43:24 -0300

    back to master

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -3,18 +3,10 @@ const Dev = require("../models/Devs");
 module.exports = {
   async index(req, res) {
     const { techs } = req.query;
-
-    //const techsArray = parseStringAsArray(techs);
-
-    /*const devs = await Dev.find({
-      techs: {
-        $in: techsArray
-      }
-    });*/
     const devs = await Dev.find(
       { $text: { $search: techs } },
       { score: { $meta: "textScore" } }
-   ).sort( { score: { $meta: "textScore" } } )
+    ).sort({ score: { $meta: "textScore" } });
     return res.json({ devs });
   },
 };
